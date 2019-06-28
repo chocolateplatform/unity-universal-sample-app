b83a5510b6ce49dc7081311f0b2d7c851cb7c6c5
Automatic CI Releaser <ltrubov@chocolateplatform.com>
2019-06-28 15:56:46 -0700

    proper name for file, credentials for upload

--- scripts/upload_unity_package.rb
+++ scripts/upload_unity_package.rb
@@ -8,11 +8,13 @@ require 'json'
 SCRIPT_DIR = File.expand_path(File.dirname(__FILE__))
 RLS_TAGN = '_release_v'
 AWS_LOCN = 's3://vdopia-sdk-files'
-PKG_NAME = 'ChocolatePlatformAds.unitypackage'
+PKG_NAME = 'ChocolatePlatformAds'
+PKG_EXTN = '.unitypackage'
+PKG_FILE = "#{PKG_NAME}#{PKG_EXTN}"
 VER_FILE = "#{SCRIPT_DIR}/versions.json"
 
 def versioned_file(version)
-  "#{PKG_NAME}#{RLS_TAGN}#{version}"
+  "#{PKG_NAME}#{RLS_TAGN}#{version}#{PKG_EXTN}"
 end
 
 ## -- Running script --
@@ -20,5 +22,5 @@ end
 vers = JSON.parse(File.read(VER_FILE))
 exit if vers['ignore']
 
-target = versioned_file(vers['version'])
-%x(aws s3 cp #{PKG_NAME} #{AWS_LOCN}/#{target})
+target = versioned_file(vers['plugin-unity'])
+%x(aws s3 cp #{PKG_FILE} #{AWS_LOCN}/#{target})
