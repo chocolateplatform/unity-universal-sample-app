8628cf5e8814fbfe004756cd8fa3d92fe54e7a84
Automatic CI Releaser <ltrubov@chocolateplatform.com>
2019-06-27 17:45:28 -0700

    upload script

--- scripts/upload_unity_package.rb
+++ scripts/upload_unity_package.rb
@@ -3,20 +3,22 @@
 # Author: Lev Trubov
 # © 2019 Vdopia, Inc.
 
-PROJ_DIR = 'SampleApp'
-UNITY_EX = '/Applications/Unity/Unity.app/Contents/MacOS/Unity'
-PKG_NAME = 'ChocolatePlatformAds.unitypackage'
+require 'json'
 
-def build
-  cmd = "(cd #{PROJ_DIR} && "
-  cmd += "#{UNITY_EX} -nographics -batchmode -quit -projectPath `pwd` "
-  cmd += "-exportPackage Assets/Plugins Assets/Scripts Assets/Editor "
-  cmd += "#{ENV['TMPDIR']}#{PKG_NAME}"
-  cmd += ")"
-  puts cmd
-  system(cmd)
+SCRIPT_DIR = File.expand_path(File.dirname(__FILE__))
+RLS_TAGN = '_release_v'
+AWS_LOCN = 's3://vdopia-sdk-files'
+PKG_NAME = 'ChocolatePlatformAds.unitypackage'
+VER_FILE = "#{SCRIPT_DIR}/sdk_versions.json"
+
+def versioned_file(version)
+  "#{PKG_NAME}#{RLS_TAGN}#{vers}"
 end
 
 ## -- Running script --
 
-build
+vers = JSON.parse(File.read(VER_FILE))
+exit if vers['ignore']
+
+target = versioned_file(vers['version'])
+%x(aws s3 cp #{PKG_NAME} #{AWS_LOCN}/#{target})
